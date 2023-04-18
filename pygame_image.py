655b0fa81e9a7d1eea2f10217164ae99b33e7483
Seiya.W <c0b2216422@edu.teu.ac.jp>
2023-04-18 16:46:56 +0900

    最終

--- pygame_image.py
+++ pygame_image.py
@@ -6,6 +6,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    bg_img_re = pg.transform.flip(bg_img, True, False)
     
     kk_image = pg.image.load("ex01/fig/3.png")
     kk_image = pg.transform.flip(kk_image, True, False)
@@ -16,15 +17,17 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
+        
+        
         tmr += 1
-        x = tmr % 1600
+        x = tmr % 3200
         screen.blit(bg_img, [-x, 0])
-        screen.blit(bg_img, [1600 - x, 0])
+        screen.blit(bg_img_re, [1600 - x, 0])
+        screen.blit(bg_img, [3200 - x, 0])
         screen.blit(kk_lst[(tmr // 50) % 2], [300, 200])
         
         pg.display.update()
-        clock.tick(100)
+        clock.tick(500)
 
 
 if __name__ == "__main__":
