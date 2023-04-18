62f29ee3ed915c59eda461246bf7f0693aad7e7d
Seiya.W <c0b2216422@edu.teu.ac.jp>
2023-04-18 16:24:19 +0900

    5問目まで

--- pygame_image.py
+++ pygame_image.py
@@ -19,7 +19,8 @@ def main():
 
         tmr += 1
         screen.blit(bg_img, [0, 0])
-
+        screen.blit(kk_lst[tmr % 2], [300, 200])
+        
         pg.display.update()
         clock.tick(100)
 
